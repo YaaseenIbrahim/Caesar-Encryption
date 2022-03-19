e2d2fae158d3f96a24071e72b6dea21501364736
YaaseenIbrahim <yaseenibrahimm12@gmail.com>
2022-03-19 07:26:43 +0500

    Initializing

--- Caesar Encryption.py
+++ Caesar Encryption.py
@@ -5,35 +5,36 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 print("Welcome to Caeser cipher encryptor\n")
 run = True
 while run:
-    enterDirection = input("Type 'encode' to encrypt and 'decode' to decrypt data:\n").lower()
-    text = input("\nEnter the message:\n").lower()
-    keyValid = True
-    while keyValid:
-        msgArray = text.split(" ")
-        indexOf_Array = len(msgArray) - 1
-        finalElement = msgArray[indexOf_Array]
-        step = int(finalElement)
-        if step % 26 == 0:
-            print("Step/key number invalid, kindly enter another key\n")
-        else:
-            keyValid = False
-
-    def caeser(start_text, shift_amount, cipher_direction):
-        end_text = ""
-        if cipher_direction == "decode":
-            shift_amount *= -1
-        for i in start_text:
-            if i in alphabet:
-                position = alphabet.index(i)
-                new_pos = position + shift_amount
-                end_text += alphabet[new_pos]
-            else:
-                end_text += i
-        print(f"Your {cipher_direction}d data = {end_text}")
-    step = step % 26
+    enterDirection = input(
+        "Type 'encode' to encrypt and 'decode' to decrypt data:\n").lower()
     if enterDirection == "encode" or enterDirection == "decode":
+        text = input("\nEnter the message:\n").lower()
+        keyValid = True
+        while keyValid:
+            msgArray = text.split(" ")
+            step = int(msgArray[-1])
+            if step % 26 == 0:
+                print("Step/key number invalid, kindly enter another key\n")
+            else:
+                keyValid = False
+
+        def caeser(start_text, shift_amount, cipher_direction):
+            end_text = ""
+            if cipher_direction == "decode":
+                shift_amount *= -1
+            for i in start_text:
+                if i in alphabet:
+                    position = alphabet.index(i)
+                    new_pos = position + shift_amount
+                    end_text += alphabet[new_pos]
+                else:
+                    end_text += i
+            print(f"Your {cipher_direction}d data = {end_text}")
+        step = step % 26
+
         caeser(text, step, enterDirection)
-        choice = input("\nDo you want to Continue encrypting? Type 'Y' or 'N':\n").lower()
+        choice = input(
+            "\nDo you want to Continue encrypting? Type 'Y' or 'N':\n").lower()
         if choice == "n":
             run = False
     else:
