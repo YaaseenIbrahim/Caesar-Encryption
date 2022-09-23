39423682af5ad9576dbf3cf2f56d5151f7ea2a35
YaaseenIbrahim <yaseenibrahimm12@gmail.com>
2022-09-23 14:53:40 +0500

    Technical changes -Top sections height is determined at windows loadup and depends on screensize -Small size sharpness adjusted -After 500px width, diamonds should adjust based on vw (so should the text and keys)

--- website/script.js
+++ website/script.js
@@ -1,4 +1,4 @@
-
+// Giving flashlight animation 
 window.addEventListener('load', function () {
     document.querySelector(".overlay").classList.add("startup");
 
@@ -7,3 +7,34 @@ window.addEventListener('load', function () {
     }, 3000);
 })
 
+
+const topSection = document.querySelector("#topSection")
+const windowHeight = (screen.height) - 110;
+topSection.style.height =  windowHeight + "px"; 
+
+// const DiamondSvg = Array.from(document.querySelectorAll(".diamondsvg"))
+// const Options = document.querySelector(".options");
+// window.onresize = window.onload = function () {
+
+//     if (window.innerWidth < 510){
+//         DiamondSvg.forEach(diamond => {
+//             diamond.classList.add("scalarDiamond")
+//             Options.classList.add("scalarOptions")
+//             Options.classList.remove("options")
+
+//         });
+
+//     }
+//     else{
+//         DiamondSvg.forEach(diamond => {
+//             diamond.classList.remove("scalarDiamond")
+//             Options.classList.remove("scalarOptions")
+//             Options.classList.add("options")
+
+//         });
+
+//     }
+//     console.log(Options.classList)
+
+
+// }
