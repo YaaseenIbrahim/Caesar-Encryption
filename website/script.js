dc4f55fad3e3b053a81b6e7b9475267214b19506
YaaseenIbrahim <yaseenibrahimm12@gmail.com>
2022-10-15 15:47:16 +0500

    Webpage Fully Responsive -Mobile version has Round rectangles for the options instead of diamonds -The asymkey changes to the darker green color in mobile to match with background -JQuery added (as CDN) -Options and Option-containers have been better named -Prism now scales in mobile so it should prevent the overlay overflow -Other prism pics deleted and the one in use renamed to "Prism" -Zooming would still cause issues

--- website/script.js
+++ website/script.js
@@ -7,8 +7,26 @@ window.addEventListener('load', function () {
     }, 3000);
 })
 
-
 const topSection = document.querySelector("#topSection")
 const TopToolbars = window.outerHeight - window.innerHeight
 const windowHeight = (window.screen.availHeight) - TopToolbars;
-topSection.style.height =  windowHeight + "px"; 
+topSection.style.height = windowHeight + "px";
+
+// Mobile to PC switches
+window.onload = window.onresize = function () {
+
+    const wrapping = $(".wrapping")
+    const KeyTextContainerHTML = "<div class=\"keyTextContainer flex-parent\"></div>"
+    if (window.innerWidth <= 500 && wrapping.parent().is($(".keyTextContainer"))) {
+        wrapping.unwrap()
+        console.log($("[asymkey]").src)
+        $("[asymkey]").attr("src", "website/Pics/key2-mobile.svg")
+    }
+    else if (window.innerWidth > 500 && wrapping.parent().is($(".options"))) {
+        wrapping.slice(0, 2).wrapAll(KeyTextContainerHTML)
+        wrapping.slice(2).wrapAll(KeyTextContainerHTML)
+        $("[asymkey]").attr("src", "website/Pics/key2.svg")
+
+    }
+
+}
